928cce74f3f4bf762632c17bded4e761738b0b5a
Subir Ghosh <subirsgghosh@gmail.com>
2023-06-22 14:20:11 +0530

    first

---
+++ src/asset/data.js
@@ -0,0 +1,662 @@
+const data = [
+  {
+   "Name": "Abhishek Kumar",
+   "Phone": "8280040487",
+   "cc": "Mr. Suraj Kumar",
+   "admin": "Mr. Vicky Kumar",
+   "admin_phone": "1234567890",
+   "hostel": "Ganga",
+   "room": "105",
+   "bed": 2
+  },
+  {
+   "Name": "Abhishek Kumar",
+   "Phone": "7903930473",
+   "cc": "Mr. Suraj Kumar",
+   "admin": "Mr. Vicky Kumar",
+   "admin_phone": "1234567890",
+   "hostel": "Ganga",
+   "room": "106",
+   "bed": 2
+  },
+  {
+   "Name": "Vishwajeet Kumar",
+   "Phone": "7903799033",
+   "cc": "Mr. Suraj Kumar",
+   "admin": "Mr. Vicky Kumar",
+   "admin_phone": "1234567890",
+   "hostel": "Ganga",
+   "room": "219",
+   "bed": 1
+  },
+  {
+   "Name": "Prabhakar Kumar",
+   "Phone": "7004190076",
+   "cc": "Mr. Suraj Kumar",
+   "admin": "Mr. Vicky Kumar",
+   "admin_phone": "1234567890",
+   "hostel": "Bramhaputra",
+   "room": "312",
+   "bed": 1
+  },
+  {
+   "Name": "Ankit kumar singh",
+   "Phone": "9101357355",
+   "cc": "Mr. Suraj Kumar",
+   "admin": "Mr. Kaustub Ranjan",
+   "admin_phone": "7412589630",
+   "hostel": "Ganga",
+   "room": "312",
+   "bed": 1
+  },
+  {
+   "Name": "Pallav kumar",
+   "Phone": "8014640511",
+   "cc": "Mr. Suraj Kumar",
+   "admin": "Mr. Vicky Kumar",
+   "admin_phone": "1234567890",
+   "hostel": "Ganga",
+   "room": "105",
+   "bed": 2
+  },
+  {
+   "Name": "Aparajita",
+   "Phone": "9717813886",
+   "cc": "Mr. Suraj Kumar",
+   "admin": "Mr. Vicky Kumar",
+   "admin_phone": "1234567890",
+   "hostel": "Ganga",
+   "room": "105",
+   "bed": 2
+  },
+  {
+   "Name": "Shivangi Utkarsh",
+   "Phone": "9097804343"
+  },
+  {
+   "Name": "Sushrut Pandey",
+   "Phone": "7999166719"
+  },
+  {
+   "Name": "SNEHA KUMARI",
+   "Phone": "8873542778"
+  },
+  {
+   "Name": "ALOK KUMAR",
+   "Phone": "6200247290"
+  },
+  {
+   "Name": "KRISHNAKANT CHAUBEY",
+   "Phone": "9876454633"
+  },
+  {
+   "Name": "PIYUSH KUMAR PANDEY",
+   "Phone": "8340559490"
+  },
+  {
+   "Name": "BHAVNA RAJPUT",
+   "Phone": "7983304867"
+  },
+  {
+   "Name": "PRIYANKA MISHRA",
+   "Phone": "6201091162"
+  },
+  {
+   "Name": "PRITAM KUMAR YADAV",
+   "Phone": "7903674919"
+  },
+  {
+   "Name": "Mr. Ranajeet Kumar",
+   "Phone": 8730852571
+  },
+  {
+   "Name": "Mr. Manish Pratap Singh",
+   "Phone": 8709040188
+  },
+  {
+   "Name": "Mr. Rahul Kumar",
+   "Phone": 7735834292
+  },
+  {
+   "Name": "Dipshikha Narayan",
+   "Phone": 8826064328
+  },
+  {
+   "Name": "Mr. Keshab Kumar",
+   "Phone": 8436874158
+  },
+  {
+   "Name": "Mr. Nishant Kumar",
+   "Phone": 7903841657
+  },
+  {
+   "Name": "Mr. Ashok Kumar Chaudhary",
+   "Phone": 9939930486
+  },
+  {
+   "Name": "Kumari Kanchan",
+   "Phone": 8789902803
+  },
+  {
+   "Name": "Sakshiwala",
+   "Phone": 9110078543
+  },
+  {
+   "Name": "Abhinah Kumar",
+   "Phone": 8789902803
+  },
+  {
+   "Name": "Muhammad Aslam",
+   "Phone": 7906532356
+  },
+  {
+   "Name": "Suryakant Kumar",
+   "Phone": 9078634210
+  },
+  {
+   "Name": "Ritesh Kumar",
+   "Phone": 8095772599
+  },
+  {
+   "Name": "Hrishikesh",
+   "Phone": 8920974788
+  },
+  {
+   "Name": "Dr. Vaishali",
+   "Phone": 8789427185
+  },
+  {
+   "Name": "Mukesh Kumar",
+   "Phone": 8460965569
+  },
+  {
+   "Name": "Mukesh Kumar",
+   "Phone": 9122693729
+  },
+  {
+   "Name": "Priti Kumari",
+   "Phone": 8789036542
+  },
+  {
+   "Name": "Dr. Ravindra kumar",
+   "Phone": 8600019774
+  },
+  {
+   "Name": "Vikram Kumar",
+   "Phone": 9990681804
+  },
+  {
+   "Name": "Vinay shankar kumar",
+   "Phone": 9547494439
+  },
+  {
+   "Name": "Raju kumar sharma",
+   "Phone": 8109608933
+  },
+  {
+   "Name": "KUMAR ABHINESH",
+   "Phone": 9123203148
+  },
+  {
+   "Name": "MANOJ KUMAR",
+   "Phone": 7050236618
+  },
+  {
+   "Name": "Anjanee Kumar",
+   "Phone": 8340282094
+  },
+  {
+   "Name": "Amarjeet Paswan",
+   "Phone": 9540512812
+  },
+  {
+   "Name": "shivam kumar",
+   "Phone": 8350929615
+  },
+  {
+   "Name": "Lucky kumar",
+   "Phone": 8252685560
+  },
+  {
+   "Name": "ANCHAL ANAND",
+   "Phone": 8987255772
+  },
+  {
+   "Name": "PRIYANKA KUMARI",
+   "Phone": 7645082770
+  },
+  {
+   "Name": "Prof. Priyesh Kumar",
+   "Phone": 9560556178
+  },
+  {
+   "Name": "Krishna Raj",
+   "Phone": 7490973277
+  },
+  {
+   "Name": "Randhir Kumar",
+   "Phone": 7004769996
+  },
+  {
+   "Name": "Vikash kumar",
+   "Phone": 7004994269
+  },
+  {
+   "Name": "Priya Kumari",
+   "Phone": 7995261962
+  },
+  {
+   "Name": "Abhishek kumar",
+   "Phone": 9631521485
+  },
+  {
+   "Name": "Neha choudhary",
+   "Phone": 6205973439
+  },
+  {
+   "Name": "Sunaina kumari",
+   "Phone": 9487105974
+  },
+  {
+   "Name": "Punam verma",
+   "Phone": 8093284948
+  },
+  {
+   "Name": "Jayanti prabha bharti",
+   "Phone": 9711303992
+  },
+  {
+   "Name": "Farhat Jahan",
+   "Phone": 6287717004
+  },
+  {
+   "Name": "Suraj prakash",
+   "Phone": 8578092999
+  },
+  {
+   "Name": "Anusha Rani",
+   "Phone": 9557080490
+  },
+  {
+   "Name": "Shailesh Kumar",
+   "Phone": 7906209274
+  },
+  {
+   "Name": "Santosh Kumar",
+   "Phone": 7838452886
+  },
+  {
+   "Name": "Gautam Kumar",
+   "Phone": 8449629081
+  },
+  {
+   "Name": "Ms. Akanksha chaudhari",
+   "Phone": 8931093155
+  },
+  {
+   "Name": "Mrs. Jigyasa kumari",
+   "Phone": 8409482601
+  },
+  {
+   "Name": "Mr. Kumar Keshav Jha",
+   "Phone": 9801664785
+  },
+  {
+   "Name": "Mr. Sunny kumar",
+   "Phone": 9981868620
+  },
+  {
+   "Name": "Amit kumar",
+   "Phone": 7983155794
+  },
+  {
+   "Name": "Chandan Kumar",
+   "Phone": 6203492415
+  },
+  {
+   "Name": "Sweta Kumari",
+   "Phone": 9905618148
+  },
+  {
+   "Name": "Navdeep Pandey",
+   "Phone": 9084063221
+  },
+  {
+   "Name": "Deva nand patel",
+   "Phone": 9170124920
+  },
+  {
+   "Name": "Omprakash Kumar",
+   "Phone": 8113037908
+  },
+  {
+   "Name": "Dr. Sumit Raj",
+   "Phone": 8981105842
+  },
+  {
+   "Name": "Dipak kr Singh",
+   "Phone": 9339041782
+  },
+  {
+   "Name": "Mohit Kumar",
+   "Phone": 8984565472
+  },
+  {
+   "Name": "Dheeraj Kumar",
+   "Phone": 9999426401
+  },
+  {
+   "Name": "Satyam Kumar Singh",
+   "Phone": 7091683638
+  },
+  {
+   "Name": "Abhimanyu Kumar",
+   "Phone": 9476594379
+  },
+  {
+   "Name": "Abhishek Kumar",
+   "Phone": 9631052383
+  },
+  {
+   "Name": "Rajeev Ranjan",
+   "Phone": 7008328849
+  },
+  {
+   "Name": "Pankaj Kumar",
+   "Phone": 9993648196
+  },
+  {
+   "Name": "Brajesh Kumar",
+   "Phone": 8298617324
+  },
+  {
+   "Name": "Anand Kumar",
+   "Phone": 9955861302
+  },
+  {
+   "Name": "Dr. Sunita Sinha",
+   "Phone": 8271742429
+  },
+  {
+   "Name": "Ankita Kumari",
+   "Phone": 8210063415
+  },
+  {
+   "Name": "Chitranjan Kumar",
+   "Phone": 7011015937
+  },
+  {
+   "Name": "Mr. Chandan Kumar",
+   "Phone": "+91-9598972664"
+  },
+  {
+   "Name": "Mr. Sanjeev Kumar",
+   "Phone": 7903657802
+  },
+  {
+   "Name": "Mr. Ashish Anand",
+   "Phone": 7001947441
+  },
+  {
+   "Name": "Mr. Vivek Kumar",
+   "Phone": 7903371522
+  },
+  {
+   "Name": "Priyatam Kumar",
+   "Phone": 9465131835
+  },
+  {
+   "Name": "Kumar Vimal",
+   "Phone": 8510096046
+  },
+  {
+   "Name": "Dharmendra Kumar",
+   "Phone": 9430552550
+  },
+  {
+   "Name": "Raj Kumar",
+   "Phone": 9123439207
+  },
+  {
+   "Name": "Gaurav Anand",
+   "Phone": 7991125787
+  },
+  {
+   "Name": "Raj Kumar Sharma",
+   "Phone": 7905165567
+  },
+  {
+   "Name": "Vikash Kumar",
+   "Phone": 7586028385
+  },
+  {
+   "Name": "Prity Kumari",
+   "Phone": 6206250359
+  },
+  {
+   "Name": "Vijay kumar Choudhary",
+   "Phone": 9852813653
+  },
+  {
+   "Name": "Jitendra Kumar",
+   "Phone": 8384002991
+  },
+  {
+   "Name": "Amit Kumar",
+   "Phone": 8676869648
+  },
+  {
+   "Name": "Sujeet Suman",
+   "Phone": 9507644675
+  },
+  {
+   "Name": "Ravi Kumar",
+   "Phone": 7906795919
+  },
+  {
+   "Name": "Abhishek Sharma",
+   "Phone": 8952010788
+  },
+  {
+   "Name": "Nidhi Devi",
+   "Phone": 8756142636
+  },
+  {
+   "Name": "Ekta Tripathi",
+   "Phone": 8948603008
+  },
+  {
+   "Name": "Shweta Kumari",
+   "Phone": 8056248921
+  },
+  {
+   "Name": "Sneha Bharti",
+   "Phone": 8310292420
+  },
+  {
+   "Name": "Ramsevak Kushwaha",
+   "Phone": 7275133894
+  },
+  {
+   "Name": "Pradhumn Tiwari",
+   "Phone": 8770929165
+  },
+  {
+   "Name": "Ram Pratap Singh",
+   "Phone": 7065866981
+  },
+  {
+   "Name": "Sikander Singh",
+   "Phone": 9532688679
+  },
+  {
+   "Name": "Sri Manan Jee",
+   "Phone": 9006529935
+  },
+  {
+   "Name": "Sri DeepakKumar",
+   "Phone": 9882743420
+  },
+  {
+   "Name": "Nitish Kumar",
+   "Phone": 9990169965
+  },
+  {
+   "Name": "Kumar Ask",
+   "Phone": 9798152615
+  },
+  {
+   "Name": "Abhimanyu Mandal",
+   "Phone": 7000374740
+  },
+  {
+   "Name": "Hare Krishna Mishra",
+   "Phone": 9667152999
+  },
+  {
+   "Name": "Ashish Kumar Suman",
+   "Phone": 9773823121
+  },
+  {
+   "Name": "Mithlesh Kumar",
+   "Phone": 9088376420
+  },
+  {
+   "Name": "Akash Kashyap",
+   "Phone": 9472001518
+  },
+  {
+   "Name": "kumar Saurabh",
+   "Phone": 8789930449
+  },
+  {
+   "Name": "Sanjeev Kumar Sajjan",
+   "Phone": 7895379560
+  },
+  {
+   "Name": "Reshu Singh",
+   "Phone": 8056107811
+  },
+  {
+   "Name": "Abhishek Kumar",
+   "Phone": 7044012955
+  },
+  {
+   "Name": "Neha Rani",
+   "Phone": 9576870131
+  },
+  {
+   "Name": "Pragya Chaudhary",
+   "Phone": 7320023165
+  },
+  {
+   "Name": "REENA KUMARI",
+   "Phone": 7735953461
+  },
+  {
+   "Name": "Raunaque Paraveen",
+   "Phone": 6307081107
+  },
+  {
+   "Name": "Poonam Kumari",
+   "Phone": 9731040865
+  },
+  {
+   "Name": "RAVI BHUSHAN KUMAR",
+   "Phone": 8102888355
+  },
+  {
+   "Name": "BABY KHUSHBOO",
+   "Phone": 8709801813
+  },
+  {
+   "Name": "Ajay Kumar",
+   "Phone": 8678816231
+  },
+  {
+   "Name": "Mantu Kumar Ram",
+   "Phone": 9572420709
+  },
+  {
+   "Name": "Jitendra Kumar",
+   "Phone": 9955538885
+  },
+  {
+   "Name": "Navneet Kumar Singh",
+   "Phone": 9717126708
+  },
+  {
+   "Name": "Vicky Kumar",
+   "Phone": 7992274025
+  },
+  {
+   "Name": "Avinash Kumar",
+   "Phone": 8507110665
+  },
+  {
+   "Name": "Aditya Raj",
+   "Phone": 8507679489
+  },
+  {
+   "Name": "Amar Kumar",
+   "Phone": 9508715069
+  },
+  {
+   "Name": "Gaurav Kumar Singh",
+   "Phone": 8863076490
+  },
+  {
+   "Name": "Prince Raj",
+   "Phone": 7903430646
+  },
+  {
+   "Name": "Rajnish kumar",
+   "Phone": 8709223575
+  },
+  {
+   "Name": "Abhash Ranjan",
+   "Phone": 7091993693
+  },
+  {
+   "Name": "Manish Kumar",
+   "Phone": 8083158093
+  },
+  {
+   "Name": "Rashmi Rani",
+   "Phone": 9955834470
+  },
+  {
+   "Name": "Ishant Kumar",
+   "Phone": 8864857438
+  },
+  {
+   "Name": "Praful Chand",
+   "Phone": 8884911159
+  },
+  {
+   "Name": "Poonam kumari",
+   "Phone": 7384708466
+  },
+  {
+   "Name": "Bablesh kumar jha",
+   "Phone": 9454196195
+  },
+  {
+   "Name": "Diwakar kumar",
+   "Phone": 7870763953
+  },
+  {
+   "Name": "Amit kumar singh",
+   "Phone": 9123152659
+  },
+  {
+   "Name": "Abhinav kumar",
+   "Phone": 7003389681
+  },
+  {
+   "Name": "Nikhil kumar",
+   "Phone": 9471477486
+  }
+ ]
+
+ export default data;
---
+++ src/components/Home.jsx
@@ -0,0 +1,134 @@
+import React, { useState } from 'react';
+import data from '../asset/data';
+import bipard from "../asset/BIPARD.jpg";
+import { Button, Form, Row, Col} from "react-bootstrap";
+import "./home.css";
+
+const Home = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResult, setSearchResult] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+    setError(null); // Reset error when input changes
+  };
+
+  const handleSearch = () => {
+    if (searchTerm.trim() === '') {
+      setError("Please enter a valid search term");
+      setSearchResult(null);
+      return;
+    }
+
+    const result = data.find((item) => item.Phone === searchTerm);
+
+    if (result) {
+      setSearchResult(result);
+      setError(null);
+    } else {
+      setSearchResult(null);
+      setError('No matching details found');
+    }
+  };
+
+  return (
+    <div className='container-sm'>
+      <Row className='search'>
+        <Col sm={4}>
+          <Form className="d-flex">
+      <Form.Control
+        type="search"
+        placeholder="Enter Your Mobile Number:"
+        className="me-2 rounded-pill"
+        aria-label="Enter Your Mobile Number:"
+        value={searchTerm}
+        onChange={handleInputChange}
+        maxLength={10}
+      />
+      <div>
+        <Button variant="outline-primary" onClick={handleSearch}>Search</Button>
+        </div>
+        </Form>
+        </Col>
+      </Row>
+      
+     
+      {error && <p>{error}</p>}
+
+      {searchResult && (
+        <>
+      
+        <img src={bipard} alt="" width="120" height="120" />
+        <h3>Bihar Institute Of Public Administration & Rural Development</h3>
+        <h4>Gaya</h4>
+        <h2 className='text-info'>Hi, <span className='text-danger'>{searchResult.Name}</span> Welcome to BIPARD</h2>
+        <table className='table table-hover table-success'>
+          <tbody className='text-start fs-4 fw-bold'>
+          <tr>
+              <td>
+                Name
+              </td>
+              <td>{searchResult.Name}</td>
+            </tr>
+            <tr>
+              <td>
+                Designation
+              </td>
+              <td>Assistant Professor</td>
+            </tr>
+            <tr>
+              
+              <td>
+                Your Course Coordinater
+              </td>
+              <td>{searchResult.cc}</td>
+            </tr>
+            <tr>
+              <td>
+                Your Admin
+              </td>
+              <td>{searchResult.admin}</td>
+            </tr>
+            <tr>
+              <td>
+                Admin Contact No
+              </td>
+              <td>
+                {searchResult.admin_phone}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Your Hostel Name
+              </td>
+              <td>{searchResult.hostel}</td>
+            </tr>
+            <tr>
+              <td>
+                Room No
+              </td>
+              <td>
+                {searchResult.room}
+              </td>
+            </tr>
+            <tr>
+              <td>
+                Bed No
+              </td>
+              <td>
+                {searchResult.bed}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        
+        
+      
+
+    </>
+      )}
+    </div>
+  );
+};
+export default Home;
