b362f0bd215de719f8bc1e536357e94631acf41b
Subir Ghosh <subirsgghosh@gmail.com>
2023-07-15 13:13:28 +0530

    assistant proffessor 6

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -135,26 +135,10 @@ const Home = () => {
             </tr>
             <tr>
               <td>
-                Bed No
-              </td>
-              <td>
-                {searchResult.Bed}
-              </td>
-            </tr>
-            <tr>
-              <td>
                 Your Allocated Mess
               </td>
               <td>
-                {searchResult.Mess}
-              </td>
-            </tr>
-            <tr>
-              <td>
-                Your Class Room
-              </td>
-              <td>
-                {searchResult.Class}
+                {searchResult.Mess}&nbsp;&nbsp;<iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d883.3390925034552!2d84.97797226957483!3d24.761615216426172!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjTCsDQ1JzQxLjgiTiA4NMKwNTgnNDMuMCJF!5e1!3m2!1sen!2sin!4v1689406818579!5m2!1sen!2sin" width="150" height="100" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" title='mess'></iframe>
               </td>
             </tr>
           </tbody>
